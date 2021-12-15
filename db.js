282ab254474383d19076e629724fe8e50dd00889
Lyra Von Rose <lyravonrose@gmail.com>
2021-12-15 13:27:12 +0100

    Query part updat/might need adjustments

--- db.js
+++ db.js
@@ -18,13 +18,37 @@ module.exports.getSignatureNumbers = () => {
     const q = "SELECT COUNT(*) FROM signatures";
     return db.query(q);
 };
-module.exports.getSigners = () => {
-    const q = "SELECT first, last FROM signatures";
+//#3
+module.exports.getSigners = (
+    UserId,
+    firstName,
+    lastName,
+    userAge,
+    userCity,
+    userUrl
+) => {
+    const q = `SELECT users.id, users.first, users.last FROM users
+    LEFT JOIN user_profiles
+    ON users.id = user_profiles.id
+    INNER JOIN signatures
+    ON users.id = signatures.id`;
+    const params = [UserId, firstName, lastName, userAge, userCity, userUrl];
+    return db.query(q, params);
+};
+
+//#4
+module.exports.getSignersFromCity = (userCity) => {
+    const q = `SELECT users.id, users.first, users.last FROM users
+    LEFT JOIN user_profiles
+    ON users.id = user_profiles.id WHERE LOWER(city)= LOWER($1)`;
+    const params = [userCity];
     return db.query(q);
 };
-module.exports.addSignatures = (firstName, lastName, signature, userId) => {
-    const q = `INSERT INTO signatures (first, last, signature, user_id) VALUES ($1, $2, $3) RETURNING id`;
-    const params = [firstName, lastName, signature];
+
+//#2
+module.exports.addSignatures = (userId, signature) => {
+    const q = `INSERT INTO signatures (user_id, signature) VALUES ($1, $2) RETURNING id`;
+    const params = [userId, signature];
     return db.query(q, params);
 };
 module.exports.addUsers = (firstName, lastName, userEmail, userPassword) => {
@@ -33,20 +57,24 @@ module.exports.addUsers = (firstName, lastName, userEmail, userPassword) => {
     return db.query(q, params);
 };
 
-module.exports.getUsers = (email) => {
-    const q = "SELECT first, last, password FROM users WHERE email";
-    return db.query(q);
-};
+// module.exports.getUsers = (email) => {
+//     const q = "SELECT first, last, password FROM users WHERE email";
+//     return db.query(q);
+// };
+
+//#5
 module.exports.getUserByEmailAdress = (email) => {
-    const q = "SELECT password, id FROM users WHERE email=$1";
+    const q = `SELECT users.id, users.first, users.last, users.email, users.password FROM users
+    JOIN signatures
+    ON users.id = signatures.id
+    WHERE email=$1`;
     const params = [email];
     return db.query(q, params);
 };
 
-// db.query("SELECT * FROM actors")
-//     .then((dbResult) => {
-//         console.log("result from the database:", dbResult.rows);
-//     })
-//     .catch((err) => console.log("err in query:", err));
-
-// vulnerability concerns
+module.exports.addProfile = (userId, userAge, userCity, userUrl) => {
+    const q = `INSERT INTO user_profiles (user_id, age, city, url) VALUES ($1, $2, $3, $4)`;
+    const params = [userId, userAge, userCity, userUrl];
+    return db.query(q, params);
+    s;
+};
